2203540e85ec49b1f97318e706ff76c32e7fbd9c
Timo Furrer <tuxtimo@gmail.com>
2019-04-05 16:53:31 +0200

    Shit

---
+++ 06_webhybridapp/MovieApp/src/app/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    Response: string;
+    Title: string;
+    Plot: string;
+    Poster: string;
+    Error: string;
+    Country: string;
+    Year: string;
+    Director: string;
+}
--- 06_webhybridapp/MovieApp/src/app/search/search.page.ts
+++ 06_webhybridapp/MovieApp/src/app/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  search() {
+    let movie: Movie;
+
+    this.http.get('http://www.omdbapi.com/?apikey=38ca3104&plot=short&r=json&t=Up').subscribe(response => {
+        movie = <Movie>response;
+        if(movie.Response != "True") {
+            console.log("Error: " + movie.Error);
+        }
+        else {
+            console.log(movie);
+            this.router.navigate(['/details', {"movie": movie}]);
+        }
+    });
   }
 
 }
